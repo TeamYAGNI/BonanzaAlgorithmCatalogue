6ecb1113572d9017b7059b7fbeef10018b7416d8
Gancho Chankov <gchankov@gmail.com>
2017-07-17 06:37:06 +0300

    Testing a failing test in build.

--- tests/unit/sample.js
+++ tests/unit/sample.js
@@ -2,6 +2,6 @@ const { expect } = require('chai');
 
 describe('sample test', () => {
     it('should be 5', () => {
-        expect(5).to.be.equal(5);
+        expect(4).to.be.equal(5);
     });
 });
