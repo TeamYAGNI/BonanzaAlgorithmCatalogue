1703e06f0106ef79ca28e7cca77bd60c246fcd29
ArnaudovSt <telerikacademy2016@gmail.com>
2017-07-30 23:55:05 +0300

    submissions back button

--- public/js/submissions.js
+++ public/js/submissions.js
@@ -6,4 +6,9 @@ $(() => {
             .parent().next('pre')
             .toggleClass('hidden');
     });
+
+    $('#back').on('click', () => {
+        window.location.href = window.location.href
+        .substring(0, window.location.href.lastIndexOf('/'));
+    });
 });
